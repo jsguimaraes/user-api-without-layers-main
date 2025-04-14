1c536eac5222bdaedb2d3c99acf04adfa6709326
Jaime Guimaraes <j.souza9410@gmail.com>
2025-04-14 19:57:29 -0300

    mudanças build

--- src/main/java/br/edu/ifrs/tads/ppa/event/NewUserEvent.java
+++ src/main/java/br/edu/ifrs/tads/ppa/event/NewUserEvent.java
@@ -2,6 +2,8 @@ package br.edu.ifrs.tads.ppa.event;
 
 import java.util.List;
 
+import br.edu.ifrs.tads.ppa.model.Profile;
+
 public record NewUserEvent(
         String name,
         String handle,
